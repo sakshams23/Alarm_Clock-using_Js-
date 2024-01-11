f4ab6eca3d0e91d04f55459d9d2ad210a6838ebb
Saksham Soni <110776016+sakshams23@users.noreply.github.com>
2024-01-12 02:43:44 +0530

    JavaScript Project

---
+++ index.js
@@ -0,0 +1,78 @@
+//Shree Ganeshaye Namah
+const currentTime = document.querySelector("h1") ,
+alarm=document.querySelector(".atime");
+setbtn=document.querySelector(".button");
+selectMenu = document.querySelectorAll("select");
+
+let alarmTime="", isSet=false;
+music = new Audio("./assets/alarm.mp3");
+
+for(let i=12; i>0;i--){
+    i=i<10 ? "0" + i : i;
+    let option= `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for(let i=59; i>=0;i--){
+    i=i<10 ? "0" + i : i;
+    let option= `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for(let i=2; i>0;i--){
+    let ampm = i==1 ? "AM" : "PM";
+    i=i<10 ? "0" + i : i;
+    let option= `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+setInterval(() => {
+
+let date = new Date(),
+h=date.getHours(),
+m=date.getMinutes(),
+s=date.getSeconds(),
+ampm="AM";
+if(h>=12){
+    h=h-12;
+    ampm="PM";
+}
+//If hour value is 0, set this value to 12
+h=h == 0 ? h = 12 : h;
+//adding 0 before hr min and sec if the value is less than 10
+h = h < 10 ? "0" + h:h;
+m = m < 10 ? "0" + m:m;
+s = s < 10 ? "0" + s:s;
+currentTime.innerText =`${h}:${m}:${s} ${ampm}`;
+
+if(alarmTime==`${h}:${m} ${ampm}`){
+    music.play();
+    music.loop=true;
+    isSet=true;
+    document.getElementById("col1").disabled = true;
+    document.getElementById("col2").disabled = true;
+    document.getElementById("col3").disabled = true;
+}
+}, 1000);
+
+function setAlarm(){
+    if(isSet){
+        alarmTime="";
+        music.pause();
+        setbtn.innerText= "Set Alarm";
+        selectMenu[0].value="Hour";
+        selectMenu[1].value="Minute";
+        selectMenu[2].value="AM/PM";
+        document.getElementById("col1").disabled = false;
+        document.getElementById("col2").disabled = false;
+        document.getElementById("col3").disabled = false;
+        return isSet= false;
+    }
+    let time= `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if(time.includes("Hour")|| time.includes("Minute") || time.includes("AM/PM")){
+        return alert("Please, Select a valid time to set Alarm!");
+    }
+    alarmTime = time;
+    setbtn.innerText="Clear Alarm";
+}
+setbtn.addEventListener("click" , setAlarm);
